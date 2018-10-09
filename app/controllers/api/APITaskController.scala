c2dfb1fcf171e21ac6220ffda40787d16120c6cd
Timothy Fisher <timothyf@gmail.com>
2018-10-09 15:23:31 -0400

    added spec to test APITaskController

--- app/controllers/api/APITaskController.scala
+++ app/controllers/api/APITaskController.scala
@@ -20,6 +20,15 @@ class APITaskController @Inject()(taskService: TaskRepository, val cc: Controlle
 
   private val logger = Logger(getClass)
 
+  val taskForm = Form(
+    mapping(
+      "id" -> ignored(None: Option[Long]),
+      "name" -> nonEmptyText,
+      "comments" -> text,
+      "completed" -> boolean
+    )(Task.apply)(Task.unapply)
+  )
+
   def index = Action { implicit request: Request[AnyContent] =>
     logger.trace("index: ")
     val tasks = taskService.all
@@ -30,6 +39,21 @@ class APITaskController @Inject()(taskService: TaskRepository, val cc: Controlle
     logger.trace("index: ")
     val task = taskService.getById(id)
     Ok(Json.toJson(task.get))
+  }
+
+  def create = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(errors.errorsAsJson),
+      task => {
+        taskService.create(task)
+        Ok(Json.toJson(task))
+      }
+    )
+  }
+
+  def delete(id: Long) = Action { implicit request: Request[AnyContent] =>
+    taskService.delete(id)
+    Ok("DELETED")
   }
 
 }
